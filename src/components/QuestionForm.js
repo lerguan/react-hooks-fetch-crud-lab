9db82c38466018c1c8daeef42e32f2a37095b2b2
Leroy G <liguan698@gmail.com>
2023-06-11 21:18:35 -0700

    complete lab'

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm() {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,72 +19,57 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const itemData = {
+      prompt: formData.prompt,
+      answers: [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+      correctIndex: formData.correctIndex,
+    };
+
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    });
   }
 
   return (
     <section>
       <h1>New Question</h1>
-      <form onSubmit={handleSubmit}>
+      <form>
         <label>
           Prompt:
-          <input
-            type="text"
-            name="prompt"
-            value={formData.prompt}
-            onChange={handleChange}
-          />
+          <input type="text" name="prompt" value={formData.prompt} onChange={handleChange} />
         </label>
         <label>
           Answer 1:
-          <input
-            type="text"
-            name="answer1"
-            value={formData.answer1}
-            onChange={handleChange}
-          />
+          <input type="text" name="answer1" value={formData.answer1} onChange={handleChange} />
         </label>
         <label>
           Answer 2:
-          <input
-            type="text"
-            name="answer2"
-            value={formData.answer2}
-            onChange={handleChange}
-          />
+          <input type="text" name="answer2" value={formData.answer2} onChange={handleChange} />
         </label>
         <label>
           Answer 3:
-          <input
-            type="text"
-            name="answer3"
-            value={formData.answer3}
-            onChange={handleChange}
-          />
+          <input type="text" name="answer3" value={formData.answer3} onChange={handleChange} />
         </label>
         <label>
           Answer 4:
-          <input
-            type="text"
-            name="answer4"
-            value={formData.answer4}
-            onChange={handleChange}
-          />
+          <input type="text" name="answer4" value={formData.answer4} onChange={handleChange} />
         </label>
         <label>
           Correct Answer:
-          <select
-            name="correctIndex"
-            value={formData.correctIndex}
-            onChange={handleChange}
-          >
+          <select name="correctIndex" value={formData.correctIndex} onChange={handleChange}>
             <option value="0">{formData.answer1}</option>
             <option value="1">{formData.answer2}</option>
             <option value="2">{formData.answer3}</option>
             <option value="3">{formData.answer4}</option>
           </select>
         </label>
-        <button type="submit">Add Question</button>
+        <button type="submit" onSubmit={handleSubmit}>
+          Add Question
+        </button>
       </form>
     </section>
   );
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questionList, setQuestionList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((resp) => resp.json())
+      .then((questionList) => {
+        setQuestionList(questionList);
+      });
+  }, []);
+
+  const questionItems = questionList.map((question) => (
+    <QuestionItem key={question.id} question={question} onDeleteItem={handleDeleteItem} onChangeItem={handleChangeItem} />
+  ));
+
+  function handleDeleteItem(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((resp) => resp.json())
+      .then(() => {
+        setQuestionList(questionList.filter((question) => question.id !== id));
+      });
+  }
+
+  function handleChangeItem(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: {
+        'correctIndex': correctIndex 
+      }
+    })
+      .then((resp) => resp.json())
+      .then(() => {
+        setQuestionList(questionList);
+      });
+  }
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
